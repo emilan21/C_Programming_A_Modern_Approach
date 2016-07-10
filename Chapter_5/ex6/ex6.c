7d307a21cd00f1b2a1ed4ea8b29b3ff7b0500a2e
Eric Milan <emilan@freebsd-vm.etmilan.org>
2016-07-10 16:14:36 -0400

    still on ex6

--- Chapter_5/ex6/ex6.c
+++ Chapter_5/ex6/ex6.c
@@ -7,27 +7,33 @@ int main(void)
 	int first_digit, grp_one_one, grp_one_two, grp_one_three, grp_one_four, grp_one_five,
 	    grp_two_one, grp_two_two, grp_two_three, grp_two_four, grp_two_five,
 	    first_sum, second_sum, total, check_digit;
+	int vaild = 0;
 
+	/* get input */
 	printf("Enter the first (single) digit: ");
 	scanf("%1d", &first_digit);
+
 	printf("Enter first group of five digits: ");
 	scanf("%1d%1d%1d%1d%1d",
 		&grp_one_one, &grp_one_two, &grp_one_three, &grp_one_four, &grp_one_five);
+
 	printf("Enter second group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &grp_two_one, &grp_two_two, &grp_two_three, &grp_two_four, &grp_two_five);
+	scanf("%1d%1d%1d%1d%1d", &grp_two_one, &grp_two_two, &grp_two_three,
+	&grp_two_four, &grp_two_five);
 
-	if(first_digit < 0 || grp_one_one < 0 || grp_one_two < 0 || grp_one_three < 0 || \
-		grp_one_four < 0 || grp_one_five < 0 || grp_two_one < )
-
+	/* do calulations */
 	first_sum = first_digit + grp_one_two + grp_one_four +
-		grp_two_one + grp_two_three + grp_two_five;
+	grp_two_one + grp_two_three + grp_two_five;
+
 	second_sum = grp_one_one + grp_one_three + grp_one_five
-		+ grp_two_two + grp_two_four;
+	+ grp_two_two + grp_two_four;
+
 	total = ((3 * first_sum) + second_sum) - 1;
 
 	check_digit = 9 - (total % 10);
 
-	printf("Check digit: %d\n", check_digit );
+	printf("%s\n", check_digit ? "VAILD" : "NOT VAILD");
+
 
 	return 0;
 }
