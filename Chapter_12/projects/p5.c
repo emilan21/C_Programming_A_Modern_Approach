c110c1389b42fcb9d05665fa6ea81797587d6953
Eric Milan <emilan@dp-pc.ericmilan.com>
2022-09-08 16:40:11 -0400

    Chapter 12 p6

--- Chapter_12/projects/p5.c
+++ Chapter_12/projects/p5.c
@@ -11,6 +11,7 @@ int main(void)
         char a[SIZE] = {0};
         char ch;
         char *p = a;
+        char *t;
         char terminating_char;
         //int i = 0;
         int j = 0;
@@ -30,17 +31,23 @@ int main(void)
         for (p = a + SIZE - 1; p >= a; p--) {
             if (*p != 0) {
                 if (*p == ' ') {
+                    t = p;
                     for (j = 0; j < word_size + 1; j++) {
-                        printf("%s", p + j);
+                        printf("%c", *p);
+                        p++;
                     }
                     word_size = 0;
+                    p = t;
                 }
-                else if (*p == 0) {
+                else if (p == a) {
                     printf(" ");
+                    t = p;
                     for (j = 0; j < word_size + 1; j++) {
-                        printf("%s", p + j);
+                        printf("%c", *p);
+                        p++;
                     }
                     word_size = 0;
+                    p = t;
                     
                 }
                 else {
